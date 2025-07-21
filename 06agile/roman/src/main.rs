0bdd9d56eb3645bfe32446ed0353566bebe79e18
Ernesto Menéndez <pyalec@gmail.com>
2025-07-20 20:45:58 -0600

    06agile: TDD: roman numerals: simplify "to_roman" function logic by deduplicating some conditions

--- 06agile/roman/src/main.rs
+++ 06agile/roman/src/main.rs
@@ -37,49 +37,35 @@ fn main() {
 }
 
 fn to_roman(number: i16) -> Result<String> {
-    if number == 0 {
-        Ok("".to_owned())
-    } else if number == 1 {
-        Ok("I".to_owned())
-    } else if number == 5 {
-        Ok("V".to_owned())
-    } else if number == 10 {
-        Ok("X".to_owned())
-    } else if number == 50 {
-        Ok("L".to_owned())
-    } else if number == 100 {
-        Ok("C".to_owned())
-    } else if number == 500 {
-        Ok("D".to_owned())
-    } else if number == 1000 {
-        Ok("M".to_owned())
-    } else if number > 3999 {
+    if number < 0 || number > 3999 {
         roman_repr_error(number)
-    } else if number > 1000 {
+    } else if number == 0 {
+        Ok("".to_owned())
+    } else if number >= 1000 {
         Ok("M".to_owned() + &to_roman(number - 1000)?)
     } else if number >= 900 {
         Ok("CM".to_owned() + &to_roman(number - 900)?)
-    } else if number > 500 {
+    } else if number >= 500 {
         Ok("D".to_owned() + &to_roman(number - 500)?)
     } else if number >= 400 {
         Ok("CD".to_owned() + &to_roman(number - 400)?)
-    } else if number > 100 {
+    } else if number >= 100 {
         Ok("C".to_owned() + &to_roman(number - 100)?)
     } else if number >= 90 {
         Ok("XC".to_owned() + &to_roman(number - 90)?)
-    } else if number > 50 {
+    } else if number >= 50 {
         Ok("L".to_owned() + &to_roman(number - 50)?)
     } else if number >= 40 {
         Ok("XL".to_owned() + &to_roman(number - 40)?)
-    } else if number > 10 {
+    } else if number >= 10 {
         Ok("X".to_owned() + &to_roman(number - 10)?)
     } else if number == 9 {
         Ok("IX".to_owned())
-    } else if number > 5 {
+    } else if number >= 5 {
         Ok("V".to_owned() + &to_roman(number - 5)?)
     } else if number == 4 {
         Ok("IV".to_owned())
-    } else if number > 1 {
+    } else if number >= 1 {
         Ok("I".to_owned() + &to_roman(number - 1)?)
     } else {
         roman_repr_error(number)
